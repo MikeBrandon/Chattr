2045938f17f057cf14563069203274cf2610e0a9
Skye Drew <55151161+SkyeDrew@users.noreply.github.com>
2022-07-05 23:50:28 +0300

    created menu dropdown and button

---
+++ Frontends/Web/src/lib/icons/MenuIcon.svelte
@@ -0,0 +1,9 @@
+<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <rect width="24" height="24" fill="url(#pattern0)"/>
+    <defs>
+        <pattern id="pattern0" patternContentUnits="objectBoundingBox" width="1" height="1">
+            <use xlink:href="#image0_137_2" transform="scale(0.0416667)"/>
+        </pattern>
+        <image id="image0_137_2" width="24" height="24" xlink:href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYEAYAAACw5+G7AAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAABmJLR0QAAAAAAAD5Q7t/AAAACXBIWXMAAABgAAAAYADwa0LPAAAAB3RJTUUH5gcFEhkBepKQjQAAAP1JREFUWMPt1rFKw2AUhuH3JLVLOjm4CC7tBVgV6S0Ug3RtHaSLQxE6ZHF1sCAUIcEbEISCONT7CI4u7eTSwTGZqslxEqcK1eE4/M8VvN/2geP8iWgZN8PBaISQylMUAbfsVavWYT8453m5BDnS1/FYVON+uMgyYFfOajXrujUMeMsyD2TBVpJ8L/v3vjovOEkS6xjHsSZa3rSOW40G+EPd6HQQuePA963DVlI9JS0KKGJ5n05FyzgK0/kcYUcu63XrvjUE7M9mHiIP0lO1rvmtCvqxLZvtNuK/6FW3C3pNLwisw1ZS714P8xzhsZJPJtY5jmPN3WlD7k47DvAJtc9v8bGhyhkAAAAldEVYdGRhdGU6Y3JlYXRlADIwMjItMDctMDVUMTg6MjU6MDErMDA6MDDNlbIFAAAAJXRFWHRkYXRlOm1vZGlmeQAyMDIyLTA3LTA1VDE4OjI1OjAxKzAwOjAwvMgKuQAAACh0RVh0ZGF0ZTp0aW1lc3RhbXAAMjAyMi0wNy0wNVQxODoyNTowMSswMDowMOvdK2YAAAAASUVORK5CYII="/>
+    </defs>
+</svg>
