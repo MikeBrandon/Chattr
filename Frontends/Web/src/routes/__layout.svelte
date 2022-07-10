c76a33a8c11c3c20ad5e6c5b2151d1e49286b0c3
Mike Brandon <55140732+MikeBrandon@users.noreply.github.com>
2022-07-10 19:25:37 +0300

    add: toastManager

---
+++ Frontends/Web/src/routes/__layout.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { SvelteToast } from '@zerodevx/svelte-toast';
+
+    const options = {
+        pausable: true
+    }
+</script>
+
+<main>
+    <SvelteToast {options} />
+    <slot />
+</main>
---
+++ Frontends/Web/src/utils/toastManager.ts
@@ -0,0 +1,27 @@
+import { toast } from '@zerodevx/svelte-toast';
+
+export const successToast = (m: string, options = {}) => toast.push(m, {
+  theme: {
+    '--toastBackground': '#7EC832',
+    '--toastColor': 'white',
+    '--toastBarBackground': 'white'
+  }
+})
+
+export const warningToast = (m: string) => toast.push(m, { theme: {
+    '--toastBackground': 'orange',
+    '--toastColor': 'white',
+    '--toastBarBackground': 'white'
+} })
+
+export const failureToast = (m: string) => toast.push(m, { theme: {
+    '--toastBackground': '#E84E38',
+    '--toastColor': 'white',
+    '--toastBarBackground': 'white'
+} })
+
+export const normalToast = (m: string) => toast.push(m, { theme: {
+    '--toastBackground': '#FF914D',
+    '--toastColor': '#000000',
+    '--toastBarBackground': '#000000'
+} })
