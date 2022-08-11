a7535886de21f5bdf69842b7a40b5f0c36980175
Mike Brandon <55140732+MikeBrandon@users.noreply.github.com>
2022-08-11 17:05:12 +0300

    add: Login

---
+++ Frontends/Web/src/lib/components/LogIn/index.svelte
@@ -0,0 +1,107 @@
+<script lang="ts">
+	import GoogleLogo from '$lib/icons/GoogleLogo.svelte';
+	import Button from '../Reusables/Button.svelte';
+	import Input from '../Reusables/Input.svelte';
+	import type { LogInData, LogInResponse } from '../../../utils/types';
+	import { logInUser } from '../../../utils/api/auth';
+	import { goto } from '$app/navigation';
+
+	let email = '';
+	let password = '';
+
+	const handleLogIn = async () => {
+		const data: LogInData = {
+			email,
+			password
+		};
+		const response: LogInResponse = await logInUser(data);
+		localStorage.setItem('auth_token', response.auth_token);
+	};
+</script>
+
+<div class="sign-up">
+	<div class="inputs">
+		<div class="input-wrapper">
+			<div class="label">Email</div>
+			<Input bind:value={email} email />
+		</div>
+		<div class="input-wrapper">
+			<div class="label">Password</div>
+			<Input
+				bind:value={password}
+				password
+				placeholder="Password length must be atleast 8 characters"
+			/>
+		</div>
+	</div>
+	<div class="gap" />
+	<div class="buttons">
+		<Button on:click={handleLogIn}>Sign Up</Button>
+		<Button dark>
+			<div class="google">
+				<div class="text">Sign In with</div>
+				<div class="logo">
+					<GoogleLogo />
+				</div>
+			</div>
+		</Button>
+	</div>
+	<div class="gap" />
+	<div
+		class="extra"
+		on:click={() => {
+			goto('signup');
+		}}
+	>
+		New here? Create an account
+	</div>
+	<div class="gap" />
+	<div
+		class="extra"
+		on:click={() => {
+			goto('password-reset');
+		}}
+	>
+		Forgot Password?
+	</div>
+</div>
+
+<style lang="postcss">
+	.sign-up {
+		@apply bg-color-app-background-2;
+		@apply p-[16px] md:p-[1.67vw];
+		@apply rounded-[1.1vh] md:rounded-[1.1vw];
+		@apply m-auto;
+	}
+
+	.input-wrapper {
+		@apply text-white text-[1.25vh] md:text-[1.1vw];
+		@apply flex flex-col gap-[clamp(12px,0.83vw,0.83vw)];
+		@apply w-[31.59vh] md:w-[31.59vw];
+	}
+
+	.inputs {
+		@apply flex flex-col gap-[clamp(12px,1.11vw,1.11vw)];
+	}
+
+	.gap {
+		@apply my-[2vh] md:my-[1.67vw];
+	}
+
+	.buttons {
+		@apply flex items-center gap-[16px] md:gap-[3.61vw];
+		@apply min-h-max;
+	}
+
+	.logo {
+		@apply w-[1.11vh] h-[1.11vh] md:w-[1.11vw] md:h-[1.11vw];
+	}
+
+	.google {
+		@apply flex items-center justify-center gap-[0.56vh] md:gap-[0.56vw];
+	}
+
+	.extra {
+		@apply w-full text-center text-[1.25vh] md:text-[1.1vw] text-color-app-color cursor-pointer hover:text-white;
+	}
+</style>
--- Frontends/Web/src/utils/types.ts
+++ Frontends/Web/src/utils/types.ts
@@ -3,9 +3,24 @@ export type SignUpData = {
     email: string;
     password: string;
     confirmPassword: string;
-}
+};
+
+export type LogInData = {
+    email: string;
+    password: string;
+};
 
 export type NavLink = {
     text: string;
     href: string;
+};
+
+export type RegisterResponse = {
+    auth_token: string;
+    msg: string;
+};
+
+export type LogInResponse = {
+    auth_token: string;
+    msg: string;
 }
