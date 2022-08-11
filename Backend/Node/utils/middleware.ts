b803bfc217934705692600235353068ff9e496c0
Mike Brandon <55140732+MikeBrandon@users.noreply.github.com>
2022-08-11 23:39:53 +0300

    cleanup

--- Backend/Node/utils/middleware.ts
+++ Backend/Node/utils/middleware.ts
@@ -1,6 +1,5 @@
 const jwt = require("jsonwebtoken");
 
-
 export const verifyResetToken = (req: any, res: any, next: any) => {
   const token =
     req.body.token || req.query.token || req.headers["x-access-token"];
